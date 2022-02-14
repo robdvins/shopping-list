e010db8c38f536bb6fb99c2b304001798190ad84
Roberto Duarte <robert0d3@protonmail.com>
2022-02-14 15:44:10 -0600

    refactor: change gql queries, mutations and schema

---
+++ src/graphql/interactions.ts
@@ -0,0 +1,97 @@
+import { gql } from 'graphql-tag'
+
+type Priority = 'LOW' | 'MEDIUM' | 'HIGH'
+
+// Queries
+export interface ListByIdResponse {
+  info: {
+    _id: string
+    name: string
+  }
+}
+export const ListById = gql`
+  query ListById($list_id: String!) {
+    info: listById(id: $list_id) {
+      _id
+      name
+    }
+  }
+`
+
+export interface ProductsByListResponse {
+  products: {
+    list: {
+      _id: string
+      name: string
+      priority: Priority
+      purchased: boolean
+    }[]
+  }
+}
+export const ProductsByList = gql`
+  query ProductsByList($list_id: String!) {
+    products: productsByList(lsit_id: $list_id) {
+      list: data {
+        _id
+        name
+        priority
+        purchased
+      }
+    }
+  }
+`
+// Mutations
+export interface CreateListResponse {
+  created: {
+    id: string
+    name: string
+  }
+}
+export const CreateList = gql`
+  mutation CreateList($id: String!, $name: String!) {
+    created: createList(data: { id: $id, name: $name }) {
+      id
+      name
+    }
+  }
+`
+
+export interface CreateProductResponse {
+  created: {
+    _id: string
+    name: string
+    priority: Priority
+    purchased: boolean
+  }
+}
+export const CreateProduct = gql`
+  mutation CreateProduct($id: ID!, $name: String!, $priority: Priority!) {
+    created: createProduct(
+      data: { name: $name, priority: $priority, purchased: false, list: { connect: $id } }
+    ) {
+      _id
+      name
+      priority
+      purchased
+    }
+  }
+`
+
+export interface DeleteProductResponse {
+  deleted: {
+    _id: string
+    name: string
+    priority: Priority
+    purchased: boolean
+  }
+}
+export const DeleteProduct = gql`
+  mutation DeleteProduct($id: ID!) {
+    deleted: deleteProduct(id: $id) {
+      _id
+      name
+      priority
+      purchased
+    }
+  }
+`
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { CREATE_LIST } from '@gql/mutations'
+import { CreateList, CreateListResponse } from '@gql/interactions'
 import { useMutation } from '@vue/apollo-composable'
 import { uid } from 'uid'
 import { reactive } from 'vue'
@@ -13,7 +13,6 @@ const list = reactive({
 })
 
 const router = useRouter()
-const { mutate: createList } = useMutation(CREATE_LIST)
 
 function goToList() {
   if (!/^[a-z0-9]{11}$/gim.test(list.id)) {
@@ -34,7 +33,8 @@ function goToList() {
   })
 }
 
-async function createNewList() {
+const { mutate } = useMutation<CreateListResponse>(CreateList)
+async function createList() {
   if (!list.name) {
     list.validName = true
 
@@ -45,66 +45,62 @@ async function createNewList() {
     return
   }
 
-  const q = await createList({ id: uid(), name: list.name })
+  const response = await mutate({ id: uid(), name: list.name })
+  if (!response) return // DO nothing for now.
 
   router.push({
     name: 'ShoppingList',
     params: {
-      id: q?.data.response.id
+      id: response.data?.created.id
     }
   })
 }
 </script>
 
 <template>
-  <div class="mx-auto w-full max-w-md">
-    <div class="mt-32 flex flex-col space-y-10">
-      <h1 class="text-5xl font-bold sm:text-6xl">
-        <span class="bg-gradient-to-tr from-gray-200 to-gray-400 bg-clip-text text-transparent">
-          A more human
-        </span>
-        <br />
-        shopping list
-      </h1>
-      <div class="flex flex-col space-y-5">
-        <input
-          v-model="list.id"
-          type="text"
-          placeholder="Enter list id"
-          :class="[
-            'text-3xl placeholder:text-gray-300 focus:outline-none',
-            { 'animate-shake': list.validId }
-          ]"
-        />
-        <button
-          class="inline-flex items-center justify-center rounded-full bg-gray-800 px-6 py-3.5 text-base font-semibold text-white shadow-lg"
-          @click="goToList"
-        >
-          Go to list
-        </button>
-      </div>
-      <div class="flex items-center space-x-3">
-        <div class="flex w-full items-center"><hr class="h-px w-full border-0 bg-gray-300" /></div>
-        <span class="mb-1.5 text-lg text-gray-500">or</span>
-        <div class="flex w-full items-center"><hr class="h-px w-full border-0 bg-gray-300" /></div>
-      </div>
-      <div class="flex flex-col space-y-5">
-        <input
-          v-model="list.name"
-          type="text"
-          placeholder="Enter list name"
-          :class="[
-            'text-3xl placeholder:text-gray-300 focus:outline-none',
-            { 'animate-shake': list.validName }
-          ]"
-        />
-        <button
-          class="inline-flex items-center justify-center rounded-full bg-blue-500 px-6 py-3.5 text-base font-semibold text-white shadow-lg shadow-blue-500/30"
-          @click="createNewList"
-        >
-          Create new list
-        </button>
-      </div>
+  <div class="mx-auto mt-32 flex w-full max-w-md flex-col space-y-10">
+    <h1 class="text-5xl font-bold sm:text-6xl">
+      <span class="bg-gradient-to-tr from-gray-200 to-gray-400 bg-clip-text text-transparent">
+        A more human
+      </span>
+      <br />
+      shopping list
+    </h1>
+    <div class="flex flex-col space-y-5">
+      <input
+        v-model="list.id"
+        type="text"
+        placeholder="Enter list id"
+        :class="['text-3xl placeholder:text-gray-300 focus:outline-none', { 'animate-shake': list.validId }]"
+      />
+      <button
+        class="inline-flex items-center justify-center rounded-full bg-gray-800 px-6 py-3.5 text-base font-semibold text-white shadow-lg"
+        @click="goToList"
+      >
+        Go to list
+      </button>
+    </div>
+    <div class="flex items-center space-x-3">
+      <div class="flex w-full items-center"><hr class="h-px w-full border-0 bg-gray-300" /></div>
+      <span class="mb-1.5 text-lg text-gray-500">or</span>
+      <div class="flex w-full items-center"><hr class="h-px w-full border-0 bg-gray-300" /></div>
+    </div>
+    <div class="flex flex-col space-y-5">
+      <input
+        v-model="list.name"
+        type="text"
+        placeholder="Enter list name"
+        :class="[
+          'text-3xl placeholder:text-gray-300 focus:outline-none',
+          { 'animate-shake': list.validName }
+        ]"
+      />
+      <button
+        class="inline-flex items-center justify-center rounded-full bg-blue-500 px-6 py-3.5 text-base font-semibold text-white shadow-lg shadow-blue-500/30"
+        @click="createList"
+      >
+        Create new list
+      </button>
     </div>
   </div>
 </template>
