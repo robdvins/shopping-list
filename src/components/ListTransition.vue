6c3844711a56935dd184547451800e937bdc6858
Roberto Duarte <robert0d3@protonmail.com>
2022-02-16 19:25:53 -0600

    feat: add tabs

---
+++ src/components/ListTransition.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{ tag?: string; moveDelay: string }>()
+</script>
+
+<template>
+  <TransitionGroup
+    :tag="tag"
+    mode="out-in"
+    enter-active-class="z-40 transition duration-[600ms] ease-[cubic-bezier(.18,.89,.32,1.28)] transform-gpu"
+    enter-from-class="-translate-y-16 opacity-0"
+    enter-to-class="transforom-none opacity-100"
+    leave-active-class="absolute transition duration-[1500ms] ease-ease-[cubic-bezier(.6,-0.28,.74,.05)] transform-gpu"
+    leave-from-class="transform-none opacity-100"
+    leave-to-class="translate-y-4 rotate[4deg] blur-md opacity-0"
+    :move-class="`transition-all duration-500 ${moveDelay} ease-[cubic-bezier(.18,.89,.32,1.28)] transform-gpu`"
+  >
+    <slot></slot>
+  </TransitionGroup>
+</template>
--- src/views/ShoppingList.vue
+++ src/views/ShoppingList.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { ListById, ListByIdResponse, ProductsByList, ProductsByListResponse } from '@gql/interactions'
-import { useQuery, useResult } from '@vue/apollo-composable'
-import ProductInput from '@components/ProductInput.vue'
 import ProductCard from '@components/ProductCard.vue'
+import ProductInput from '@components/ProductInput.vue'
+import ListTransition from '@components/ListTransition.vue'
+import { ListById, ListByIdResponse, ProductsByList, ProductsByListResponse } from '@gql/interactions'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/vue'
+import { useQuery, useResult } from '@vue/apollo-composable'
 import { computed, ref } from 'vue'
 
 const props = defineProps<{ id: string }>()
@@ -14,6 +16,9 @@ const products = useResult(productsList.result, [], (data) => data.products.list
 const unpurchased = computed(() => {
   return products.value.filter((product) => !product.purchased)
 })
+const purchased = computed(() => {
+  return products.value.filter((product) => product.purchased)
+})
 
 // This way is the closest to the animation I was looking for.
 const moveDelay = ref('delay-100')
@@ -24,25 +29,48 @@ const moveDelay = ref('delay-100')
     <div class="mx-auto mt-14 w-full max-w-md">
       <h1 class="text-2xl font-bold">{{ list?.info.name }}</h1>
       <ProductInput :fauna-id="list?.info._id || ''" :own-id="id" @created="moveDelay = 'delay-100'" />
-      <TransitionGroup
-        tag="ul"
-        enter-active-class="z-40 transition duration-[600ms] ease-[cubic-bezier(.18,.89,.32,1.28)] transform-gpu"
-        enter-from-class="-translate-y-16 opacity-0"
-        enter-to-class="transforom-none opacity-100"
-        leave-active-class="absolute transition duration-[1500ms] ease-ease-[cubic-bezier(.6,-0.28,.74,.05)] transform-gpu"
-        leave-from-class="transform-none opacity-100"
-        leave-to-class="translate-y-4 rotate[4deg] blur-md opacity-0"
-        :move-class="`transition-all duration-500 ${moveDelay} ease-[cubic-bezier(.18,.89,.32,1.28)] transform-gpu`"
-        class="relative space-y-3 pb-4"
-      >
-        <li
-          v-for="product in unpurchased"
-          :key="product._id"
-          class="flex w-full items-center rounded-xl bg-white p-3"
-        >
-          <ProductCard :own-id="id" :product="product" @deleted="moveDelay = 'delay-[800ms]'" />
-        </li>
-      </TransitionGroup>
+      <TabGroup as="div" class="h-full">
+        <TabList class="mb-3 rounded-xl bg-gray-300 p-1" #="{ selectedIndex }">
+          <div class="relative grid grid-cols-2">
+            <div
+              :style="`transform: translateX(calc(100% * ${selectedIndex}))`"
+              class="absolute inset-y-0 z-0 w-1/2 duration-300 ease-[cubic-bezier(0.97,0.04,0.01,1.02)]"
+            >
+              <span class="block h-full w-full rounded-lg bg-white"></span>
+            </div>
+            <Tab as="template">
+              <button class="relative flex justify-center py-2 focus:outline-none">Pending</button>
+            </Tab>
+            <Tab as="template">
+              <button class="relative flex justify-center py-2 focus:outline-none">Purchased</button>
+            </Tab>
+          </div>
+        </TabList>
+        <TabPanels class="min-h-full">
+          <TabPanel class="min-h-full focus:outline-none">
+            <ListTransition tag="ul" :move-delay="moveDelay" class="relative min-h-full space-y-3 pb-4">
+              <li
+                v-for="product in unpurchased"
+                :key="product._id"
+                class="flex w-full items-center rounded-xl bg-white p-3"
+              >
+                <ProductCard :own-id="id" :product="product" @deleted="moveDelay = 'delay-[800ms]'" />
+              </li>
+            </ListTransition>
+          </TabPanel>
+          <TabPanel class="min-h-full focus:outline-none">
+            <ListTransition tag="ul" :move-delay="moveDelay" class="relative min-h-full space-y-3 pb-4">
+              <li
+                v-for="product in purchased"
+                :key="product._id"
+                class="flex w-full items-center rounded-xl bg-white p-3"
+              >
+                <ProductCard :own-id="id" :product="product" @deleted="moveDelay = 'delay-[800ms]'" />
+              </li>
+            </ListTransition>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
     </div>
   </div>
 </template>
